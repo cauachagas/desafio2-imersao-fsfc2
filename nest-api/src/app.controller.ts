1893d725b40539d57f5301e35611395404101c33
Cauã Chagas <caua.geof@gmail.com>
2021-04-12 12:20:40 -0300

    nest g resource routes

--- nest-api/src/app.controller.ts
+++ nest-api/src/app.controller.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  // AppService já está registrado. 
+  // appService só precisa do tipo
+  // @Controller('/prefixo') e @Get('test') = URL/prefixo/test
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 }
+
+//HTTP - Get, Post, Put, Patch, Delete
--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { RoutesModule } from './routes/routes.module';
+//ES7 decorators
+// Ajuda na abstração
 @Module({
-  imports: [],
+  imports: [RoutesModule],
   controllers: [AppController],
   providers: [AppService],
 })
