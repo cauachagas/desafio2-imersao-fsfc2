9d0da96e7acad564421e5fd22d3e1f2dd859354c
Cauã Chagas <caua.geof@gmail.com>
2021-04-12 19:17:17 -0300

    Get routes

--- nest-api/src/routes/entities/route.entity.ts
+++ nest-api/src/routes/entities/route.entity.ts
@@ -1,1 +1,16 @@
-export class Route {}
+// Usos de Intefaces
+export interface Route {
+  title: string;
+  startPosition: StartPosition;
+  endPosition: EndPosition;
+}
+
+export interface StartPosition {
+  lat: number;
+  lng: number;
+}
+
+export interface EndPosition {
+  lat: number;
+  lng: number;
+}
--- nest-api/src/routes/routes.service.ts
+++ nest-api/src/routes/routes.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateRouteDto } from './dto/create-route.dto';
 import { UpdateRouteDto } from './dto/update-route.dto';
+import { Route } from './entities/route.entity';
 
 @Injectable()
 export class RoutesService {
-  create(createRouteDto: CreateRouteDto) {
-    return 'This action adds a new route';
-  }
+  private routes: Route[] = [
+    { 
+      title: 'Primeiro', 
+      startPosition: {lat:-15.82594, lng:-47.92923}, 
+      endPosition: {lat:-15.82942, lng:-47.92765}
+    },
+    { 
+      title: 'Segundo', 
+      startPosition: {lat:-15.82449, lng:-47.92756}, 
+      endPosition: {lat:-15.8276, lng:-47.92621}
+    },
+    { 
+      title: 'Terceiro', 
+      startPosition: {lat:-15.82331, lng:-47.92588}, 
+      endPosition: {lat:-15.82758, lng:-47.92532}
+    },
+    { 
+      title: 'Quarto', 
+      startPosition: {lat:-15.83, lng:-47.92688}, 
+      endPosition: {lat:-15.82921, lng:-47.92417}
+    },
+    { 
+      title: 'Quinto', 
+      startPosition: {lat:-15.82208, lng:-47.92519}, 
+      endPosition: {lat:-15.83061, lng:-47.92573}
+    }
+  ];
 
-  findAll() {
-    return `This action returns all routes`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} route`;
-  }
-
-  update(id: number, updateRouteDto: UpdateRouteDto) {
-    return `This action updates a #${id} route`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} route`;
+  async findAll() {
+      return this.routes
   }
 }
+
